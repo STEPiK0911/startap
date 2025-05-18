81e837b51704c11bb62d92bf4200a4f15703457e
Gennady Kirilin <kirilingd@10.211.74.58-vpn.dhcp.yndx.net>
2025-05-18 21:38:33 +0300

    test

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,17 +1,24 @@
 import { RouteProps } from "react-router-dom";
 import MainPage from "@pages/MainPage/components/MainPage";
+import AuthorizationPage from "@pages/AuthorizationPage/components/AuthorizationPage";
 
 export enum AppRoutes {
   MAIN = "main",
+  AUTH = "auth",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: "/",
+  [AppRoutes.AUTH]: "/auth",
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.MAIN]: {
     path: RoutePath.main,
     element: <MainPage />,
+  },
+  [AppRoutes.AUTH]: {
+    path: RoutePath.auth,
+    element: <AuthorizationPage />,
   },
 };
---
+++ src/widgets/AuthorizationPageWrapper/AuthorizationPageWrapper.tsx
@@ -0,0 +1,11 @@
+import InviteGraphics from "@shared/components/InviteGraphics/InviteGraphics";
+import MapWithPoint from "@shared/components/MapWithPoint/MapWithPoint";
+import SaleGraphics from "@shared/components/SaleGraphics/SaleGraphics";
+import Search from "@shared/components/Search/Search";
+import React from "react";
+
+const AuthorizationPageWrapper = () => {
+  return <div>asdasdads</div>;
+};
+
+export default AuthorizationPageWrapper;
